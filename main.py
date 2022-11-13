80d572e5d5a9147ae53bdd3524f929c481553459
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-11-13 11:44:57 -0300

    1.0
    Update no main.py foram adicionados o tamanho e a cor da janela.

--- main.py
+++ main.py
@@ -2,11 +2,24 @@
 from tkinter import *
 from tkinter import messagebox
 
-# Janela
+# Criador da Janela
 
 jan = Tk()
 jan.title("DP System - Acess")
 jan.geometry("600x300")
-jan.configure(background="white")
+jan.configure(background="black")
 jan.resizable(width=False,height=False)
 
+# Inserindo Imgs
+
+
+
+# Basicamente o separador da janela
+
+LeftFrame = Frame(jan, width=200, height=300,bg="Blue",relief="raise")
+LeftFrame.pack(side=LEFT)
+RightFrame= Frame(jan, width=395, height=300,bg="Blue",relief="raise")
+RightFrame.pack(side=RIGHT)
+
+# Chamada de funções
+jan.mainloop()
